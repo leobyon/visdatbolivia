ba44e1476fe61f8bb05fe04f0e41f541672b1cd9
vistoria <56234331+visdatbolivia@users.noreply.github.com>
2020-03-22 18:12:23 -0300

    upload shinyapp files

---
+++ bolivia_covid19_tracker/server.R
@@ -0,0 +1,60 @@
+source("Bolivia_covid19_tracker.R")
+
+
+# crear funciones de servidor 
+server <- function(input, output) { 
+  
+  #ultima fecha de actualizacion
+  output$Refresh1 <- renderText({
+    toString(paste("Última fecha de actualización: ", format(as.Date(ultima_fecha, format = "%m/%d/%y"), format = "%d/%m/%Y")))
+  })
+  
+  #caja: total confirmados
+  output$value1 <- renderValueBox({
+    valueBox(value = formatC(total_confirmados, format="d", big.mark=','),
+             subtitle = 'Total casos confirmados',
+             color = 'purple')
+    
+    
+  })
+  
+  
+  #caja: total recuperados
+  output$value2 <- renderValueBox({
+    
+    valueBox(value = formatC(total_recuperados, format="d", big.mark=','),
+             subtitle = 'Total recuperados',
+             color = 'green')
+    
+  })
+  
+  
+  #caja: total fallecidos
+  output$value3 <- renderValueBox({
+    
+    valueBox(value = formatC(total_fallecidos, format="d", big.mark=','),
+             subtitle = 'Total fallecidos',
+             color = 'yellow')
+    
+  })
+  
+  #grafico: bolivia vs ref
+  output$paisRef <- renderPlot({
+    plot_global
+  })
+  
+  #grafico: bolivia vs sudam
+  output$paisSudAm <- renderPlot({
+    plot_sudam
+  })
+  
+  
+  
+  #texto
+  output$tabset1 <- renderText({
+    
+    input$tabset1
+    
+  })
+  
+}
